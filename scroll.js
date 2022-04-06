5d6dfa4c871f80143b047ab9e1c1304dbc7d332f
Anianees <anianeesvn64@gmail.com>
2022-04-06 10:55:59 +0530

    final

---
+++ scroll.js
@@ -0,0 +1,11 @@
+document.lastscrollposition = 0;
+
+
+
+
+document.addEventListener(type: 'scroll',  listener:() => {
+const direction = window.pageYOffset - document.lastscrollposition > 0 'down' : 'up'
+
+window.pageYOffset
+}
+)
